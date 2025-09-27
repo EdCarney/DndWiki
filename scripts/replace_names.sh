5a50a39f3ac71b1f297db01375b6db01ccee9b53
Ed Carney <carneyedwardj@gmail.com>
2025-09-27 11:19:42 -0400

    Adds script to rename file

---
+++ scripts/replace_names.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# require file name
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <file_to_process>"
+    exit 1
+fi
+
+# declare name dictionary
+declare -A name_map
+
+name_map["X"]="Klaxon"
+name_map["E"]="Eryn"
+name_map["A"]="Art"
+name_map["M"]="Melody"
+name_map["K"]="Kefira"
+
+for key in "${!name_map[@]}"; do
+    sed_pattern_base="16,\$s/([^a-zA-Z0-9])$key([^a-zA-Z0-9])/\1${name_map[$key]}\2"
+    sed_pattern_print="$sed_pattern_base/gp"
+    sed_pattern_replace="$sed_pattern_base/gw $1"
+
+    # print details on what will be replaced
+    echo
+    echo "Replacing '$key' with '${name_map[$key]}'"
+    echo "Using pattern: $sed_pattern_print"
+    echo "================ REPLACEMENTS ================"
+    sed -nr $sed_pattern_print $1
+
+    # verify changes and then execute
+    echo
+    read -n 1 -s -r -p "Confirm changes then press any key to continue..."
+    sed -r $sed_pattern_replace $1
+done
+
